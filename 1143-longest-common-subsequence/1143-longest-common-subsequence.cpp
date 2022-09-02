757e6fc4d69f5c72f97e371fb83b41adbe8bef39
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-09-02 19:29:07 +0530

    Time: 34 ms (56.44%), Space: 13.1 MB (23.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -6,7 +6,11 @@ public:
         
         for(int i=1;i<=t1.size();i++){
             for(int j=1;j<=t2.size();j++){
+                // Current char is matching. Increase count by 1
                 if(t1[i-1] == t2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+                
+                // Character not match
+                // Try to move one character ahead for string one by one.
                 else dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
             }
           
