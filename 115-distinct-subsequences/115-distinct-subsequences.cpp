99f1ba2505a1012f8a9a975dcd0f316b2541e1e7
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-09-03 21:42:31 +0530

    Time: 98 ms (15.90%), Space: 18.5 MB (48.25%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,5 +1,40 @@
 class Solution {
 public:
+    // Using Iteration
+    int numDistinct(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        int prime = 1e9+7;
+        
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<m+1;i++){
+            dp[0][i]=0;
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%prime;
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+
+
+        return dp[n][m];
+    }
+};
+
+
+
+/*
+Using Recurssion
+class Solution {
+public:
     int solve(string &s, string &t, int n, int m,vector<vector<int>> &dp){
         if(m == -1 ) return 1;
         if(n == -1 ) return 0;
@@ -24,3 +59,4 @@ public:
         
     }
 };
+*/
