30246c2664ff6881f69fa6bcda3247f2ad70ab84
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-09-04 21:57:47 +0530

    Time: 25 ms (44.61%), Space: 9.1 MB (40.43%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,3 +1,38 @@
+// Interative method
+class Solution {
+public:
+    int minDistance(string w1, string w2) {
+        int l1 = w1.size(), l2 = w2.size();
+        
+        vector<vector<int>> dp(l1+1, vector<int>(l2+1, 0));
+        
+        for(int i=1;i<=l1;i++) dp[i][0] = i;
+        for(int i=1;i<=l2;i++) dp[0][i] = i;
+        
+        for(int i=1;i<=l1;i++){
+            for(int j=1;j<=l2;j++){
+                if(w1[i-1] == w2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else{
+                    int kk = INT_MAX;
+                    kk = min(kk,dp[i-1][j-1]); // Replacae OPeration
+                    kk = min(kk,dp[i-1][j]); // Delete operation
+                    kk = min(kk,dp[i][j-1]); // Insert operation
+                    dp[i][j] = kk + 1;
+                    
+                }
+            }
+        }
+        
+        return dp[l1][l2];
+
+        
+    }
+};
+
+/*
+Recurssion Method
+
 class Solution {
 public:
     int solve(string &w1, string &w2, int i, int j){
@@ -41,3 +76,4 @@ public:
         
     }
 };
+*/
