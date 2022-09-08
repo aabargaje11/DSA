4063dffb1c8c9e0fbb35e20d913cdbb6e6420207
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-09-08 22:35:13 +0530

    Time: 1496 ms (7.22%), Space: 207.1 MB (30.66%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+
+    int maxProfit(vector<int>& p) {
+        int len = p.size();
+        vector<vector<vector<int>>> dp(len+1, vector<vector<int>>(2,vector<int>(3,0)));
+        
+        for(int ind = len-1; ind>=0; ind--){
+            for(int buy = 0 ; buy < 2 ; buy++){
+                for(int b = 1 ; b <= 2; b++){
+                    int val = INT_MIN;
+                    if(buy == 1){
+                        val = max(-1 * p[ind] + dp[ind+1][0][b], dp[ind+1][1][b]);
+                    }
+                    else{
+                        val = max(p[ind] + dp[ind+1][1][b-1],dp[ind+1][0][b]);
+                    }
+                    
+                    dp[ind][buy][b] = val;
+                }
+                
+            }
+        }
+        
+        return dp[0][1][2];
+    }
+};
+
+/*
+class Solution {
+public:
     int solve(int ind, int buy, vector<int>& p, int n, int b,vector<vector<vector<int>>> &dp){
         if(ind == n or b==0) return 0;
         int val = INT_MIN;
@@ -21,3 +50,5 @@ public:
         return solve(0, 1, prices, len,2,dp);
     }
 };
+
+*/
