d15dc9653cf92f0062a6e010e5391bdb50dda1c7
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-09-09 20:39:19 +0530

    Time: 308 ms (62.57%), Space: 75.3 MB (87.04%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,29 +4,32 @@ public:
 
     int maxProfit(vector<int>& p) {
         int len = p.size();
-        vector<vector<vector<int>>> dp(len+1, vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>> after(2,vector<int>(3,0));
+        vector<vector<int>> curr(2,vector<int>(3,0));
         
         for(int ind = len-1; ind>=0; ind--){
             for(int buy = 0 ; buy < 2 ; buy++){
                 for(int b = 1 ; b <= 2; b++){
                     int val = INT_MIN;
                     if(buy == 1){
-                        val = max(-1 * p[ind] + dp[ind+1][0][b], dp[ind+1][1][b]);
+                        val = max(-1 * p[ind] + after[0][b], after[1][b]);
                     }
                     else{
-                        val = max(p[ind] + dp[ind+1][1][b-1],dp[ind+1][0][b]);
+                        val = max(p[ind] + after[1][b-1],after[0][b]);
                     }
                     
-                    dp[ind][buy][b] = val;
+                    curr[buy][b] = val;
                 }
                 
             }
+            after = curr;
         }
         
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
 
+// Using recurssion
 /*
 class Solution {
 public:
