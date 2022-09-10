5ce466405f00d5d0d7d46aca637c7836a325a1db
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-09-10 22:41:00 +0530

    Time: 14 ms (22.27%), Space: 12 MB (25.32%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int solve(vector<int>& p, int curr, int buy,vector<vector<int>> &dp, int num){
+    int solve(vector<int>& p, int curr, int buy,vector<vector<int>> &dp){
         if(curr >= p.size()){
             return 0;
         }
         int val;
         if(dp[curr][buy] != -1) return dp[curr][buy];
         if(buy){
-            val = max(-1*p[curr] + solve(p,curr+1,0,dp,num+1), solve(p, curr+1,1,dp,num));            
+            val = max(-1*p[curr] + solve(p,curr+1,0,dp), solve(p, curr+1,1,dp));            
         }
         else{
-            val = max(p[curr]+ solve(p,curr+2,1,dp,num), solve(p,curr+1,0,dp,num));
+            val = max(p[curr]+ solve(p,curr+2,1,dp), solve(p,curr+1,0,dp));
         }
         dp[curr][buy] = val;
         return val;
@@ -21,7 +21,7 @@ public:
         int len = p.size();
         vector<vector<int>> dp(len, vector<int>(2,-1));
         
-        return solve(p,0,1,dp,0);
+        return solve(p,0,1,dp);
         
     }
 };
