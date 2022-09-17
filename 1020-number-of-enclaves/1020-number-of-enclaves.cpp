701a4d1b405495605bddf487e3aa428543c57660
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-09-17 18:24:29 +0530

    Time: 178 ms (10.30%), Space: 29.3 MB (19.96%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // DFS traversal until we found the 1 in grid
     void dfs(int i, int j, vector<vector<int>>& grid){
         int r = grid.size();
         int c = grid[0].size();
@@ -18,6 +19,9 @@ public:
         int c = grid[0].size();
         int ans = 0;
         
+        
+        // For 1st and last row check if 1 is present or not
+        // If 1 is present do traversal call and mark them as zero
         for(int i = 0; i < c; i++){
             // 0 , i
             // r-1 , i
@@ -25,6 +29,8 @@ public:
             dfs(0,i,grid);
             dfs(r-1,i,grid);
         }
+        // For 1st and last column check if 1 is present or not
+        // If 1 is present do traversal call and mark them as zero
         for(int i = 0; i < r; i++){
             // i , 0
             // i , c-1
@@ -32,6 +38,8 @@ public:
             dfs(i, c-1, grid);
         }
         
+        // All grid for which we can walk to boundary are marked as zero by above
+        // two for loops. Not remaining are the one which are required for final answer
         for(int i=0;i<r;i++)
         {
             for(int j=0;j<c;j++){
