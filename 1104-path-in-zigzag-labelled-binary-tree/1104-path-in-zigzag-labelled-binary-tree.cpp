c9c5c9d010107935e636dc7d1927b6f2c981a336
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-09-27 21:26:27 +0530

    Time: 6 ms (15.30%), Space: 6.2 MB (86.63%) - LeetHub

--- 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> ans ;
         ans.push_back(label);
         
-        
+        // Calaculate height of tree
         while(temp!=1){
             temp/=2;
             count++;
@@ -15,19 +15,13 @@ public:
         
         while(count!=1){
             count--;
-            
-            // Odd
-            // if(count%2==1){
-                int start = pow(2, count-1) ;
-                int end = pow(2, count) - 1;
-                int val = temp / 2;
-                int diff = val - start;
+
+                int start = pow(2, count-1) ;  // Find starting element of previous level
+                int end = pow(2, count) - 1;   // Find ending element of previous level
+                int val = temp / 2;            //  Finf=d half
+                int diff = val - start;             
                 temp = end - diff;
                 ans.insert(ans.begin(),temp);
-            // }else{
-            //     temp = temp / 2;
-            //     ans.insert(ans.begin(),temp);
-            // }
         }
         
 
