0e02b6b8422a8e4ea8f11ab2b16097e57cb67018
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-10-25 12:10:24 -0500

    Time: 1436 ms (20.75%), Space: 164.5 MB (33.69%) - LeetHub

---
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int dfs(int node, vector<int> &dist, vector<vector<pair<int, int>>> &graph, vector<int> &dp) {
+        if(node == dp.size()-1) return 1;
+        if(dp[node] != -1) return dp[node];
+        int ans = 0;
+        for(auto nbr : graph[node]) {
+            if(dist[node] > dist[nbr.first]) {
+                ans = (ans + dfs(nbr.first, dist, graph, dp))%mod;
+            }
+        }
+        return dp[node] = ans;
+    }
+    int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+        vector<vector<pair<int, int>>> graph(n+1);
+        for(auto e : edges) {
+            graph[e[0]].push_back({e[1], e[2]});
+            graph[e[1]].push_back({e[0], e[2]});
+        }
+        vector<int> dist(n+1, INT_MAX);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, n});
+        dist[n] = 0;
+        while(!pq.empty()) {
+            pair<int, int> p = pq.top();
+            pq.pop();
+            if(dist[p.second] != p.first) 
+                continue;
+            for(auto nbr : graph[p.second]) {
+                if(dist[nbr.first] > p.first+nbr.second) {
+                    dist[nbr.first] = p.first+nbr.second;
+                    pq.push({dist[nbr.first], nbr.first});
+                }
+            }
+        }
+        vector<int> dp(n+1, -1);
+        return dfs(1, dist, graph, dp);
+    }
+};
