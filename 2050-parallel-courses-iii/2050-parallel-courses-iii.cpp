1591d2401f5fd6be62a23840e6f65c9f7cf7d427
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-10-26 10:18:20 -0500

    Time: 1098 ms (16.73%), Space: 146 MB (25.68%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -4,40 +4,38 @@ public:
         vector<vector<int>> adj(n+1,vector<int>()) ;
         vector<int> in(n+1,0);
         vector<int> start(n+1,0);
+        // Topological sort
+        // MAke adjacency list and calculate indegree
         for(auto r:relations){
             adj[r[0]].push_back(r[1]);
-            // cout<<r[0]<<r[1]<<endl;
             in[r[1]]++;
         }
         
         queue<int> q;
+        // Push values with indegree zero to queue
         for(int i=1;i<=n;i++){
             if(in[i] == 0){
                 q.push(i);
             }
         }
         
-        // vector<bool> visited(n+1, false);
         int ans = 0;
-        cout<<"---------------------------------------"<<endl;
         while(!q.empty()){
             int len = q.size();
             
             while(len!=0 and !q.empty()){
                 
                 int front = q.front();
-                // visited[front] = true;
                 
                 ans = max(ans, start[front] + time[front-1]);
                 q.pop();
-                cout<<front<<" "<<q.size()<<" "<<len<<endl;
                 for(auto num:adj[front]){
+                    // Update start time node destination node
                     start[num] = max(start[num], time[front-1] + start[front]);
+                    // decrease indegree and append to queue if value becomes zero
                     in[num]--;
                     if(in[num] == 0){
-                        q.push(num);
-                        cout<<"Push "<<num<<endl;
-                        
+                        q.push(num);                        
                     } 
                 }
                 
