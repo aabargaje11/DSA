7498cb0d038c05f7bf38f74cd5269fccec375ea4
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-10-28 23:10:58 -0500

    Time: 305 ms (77.92%), Space: 113.3 MB (28.31%) - LeetHub

--- 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     vector<int> mostCompetitive(vector<int>& nums, int k) {
         int n = nums.size();
+        
+        // How many elements we can delete
         int del = n - k;
         vector<int> s;
         
         for(int i=0;i<n;i++){
+            // if not empty and stack elements is greater than next element and we can delete
             while(!s.empty() and s.back() > nums[i] and del){
                 s.pop_back();
                 del--;
