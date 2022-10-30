b374ca7c46a9e51569179ea3cba0ecec1c0c017b
Atish Ambadas Bargaje <43517982+aabargaje11@users.noreply.github.com>
2022-10-30 18:26:26 +0530

    Time: 91 ms (61.90%), Space: 15.6 MB (37.82%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    void solve(vector<int>& heights, int &ans) {
-        int l=heights.size();
-        vector<int> leftSmall(l,0), rightSmall(l,0);
+    void solve(vector<int> heights, int &ans){
+        int n = heights.size();
+        vector<int> left(n,0),right(n,n-1);
+        // Similar to https://leetcode.com/problems/largest-rectangle-in-histogram/
+        // Largest rectangle in histogram.
+        // Find Largest rectangle in histogram for each row.
+        stack<int>s;
         
-        stack<int> s;
-        
-        for(int i=0;i<l;i++){
-            while(!s.empty() and heights[s.top()] >= heights[i] ){
+        for(int i=0;i<n;i++){
+            
+            while(!s.empty() and heights[s.top()] >= heights[i]){
                 s.pop();
             }
             
-            leftSmall[i] = (s.empty())? 0:s.top()+1;
-            //cout<<leftSmall[i];
+            left[i] = (s.empty())? 0:s.top() + 1;
             s.push(i);
         }
-        //cout<<endl;
         while(!s.empty()) s.pop();
         
-        for(int i=l-1;i>=0;i--){
-            while(!s.empty() and heights[s.top()] >= heights[i] ){
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() and heights[s.top()] >= heights[i]){
                 s.pop();
             }
-            
-            rightSmall[i] = (s.empty())? l-1:s.top()-1;
-            //cout<<rightSmall[i];
+            right[i] = (s.empty())? n-1:s.top() - 1;
             s.push(i);
         }
-        //cout<<endl;
+        int temp = 0;
         
-        for( int i=0;i<l;i++){
-            ans = max(ans, (rightSmall[i] - leftSmall[i] + 1)*heights[i]);
-            //cout<<rightSmall[i]<< leftSmall[i] <<heights[i]<<endl;
+        for(int i=0;i<n;i++){
+            int l=left[i], r = right[i];
+            
+            temp = (r-l+1)*(heights[i]);
+            ans = max(ans,temp);
         }
-
+        
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         vector<int> heights(matrix[0].size(), 0);
